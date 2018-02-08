86f56e94102c572bf52ea74ca9b1a64d1d3ae636
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-02-08 16:37:00 +0530

    Create mmm20.c

---
+++ mmm20.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i;
+    printf("Enter the value");
+    scanf("%d",&a);
+    for(i=1;i<6;i++)
+    {
+        b=i*a;
+        printf("\n%d",b);
+    }
+    
+}
+
