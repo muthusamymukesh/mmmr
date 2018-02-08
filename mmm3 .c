97bf8b8ca7e81d18129ef149054f5ba2d2ed01fa
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-02-08 16:53:58 +0530

    Create mmm3 .c

---
+++ mmm3 .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int b,a[10],i,d;
+printf("Enter the size of array:\n");
+scanf("%d",&b);
+printf("Enter the elements of array:\n");
+for(i=0;i<b;i++)
+{
+scanf("%d",&a[i]);
+}
+d=a[0];
+for(i=0;i<b;i++)
+{
+if(a[i]<c)
+{
+d=a[i];
+}}
+printf("Maximum Value is %d",d);
+}
