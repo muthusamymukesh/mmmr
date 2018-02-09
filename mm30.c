e05ff091812d69fa8e7f7aac1600486c3ae5b3d9
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-02-09 16:07:27 +0530

    Create mm30.c

---
+++ mm30.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a, b,c,d,e,f;
+    printf("Enter the time and minute :");
+    scanf("%d:%d",&a,&b); 
+    printf("Enter the time and minute :");
+    scanf("%d:%d",&c,&d);
+     e=a-c;
+     f=b-d;
+    printf("\nsubrate of time =%d:%d",e,f);
+}
